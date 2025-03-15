d50d2c9c8797ec501bfeadf7cb99b02fab1113c5
Rewant Raj <92687291+rewant06@users.noreply.github.com>
2025-03-15 13:24:18 +0530

    Dr. reach icon updated on service page

--- src/app/pages/landing/ServiceList.tsx
+++ src/app/pages/landing/ServiceList.tsx
@@ -20,7 +20,7 @@ const services = [
   },
 
   {
-    icon: "data:image/svg+xml,%3Csvg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M22 6.45798L12 13.458L2 13.458' stroke='%234A00E0' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E",
+    icon: "/drreach.png",
     title: "Dr. Reach",
     description: "",
   }
