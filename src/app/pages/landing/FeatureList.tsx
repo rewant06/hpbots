b30947411b03834ba5b5a34289c4109a1924f18e
Rewant Raj <92687291+rewant06@users.noreply.github.com>
2025-03-15 13:03:28 +0530

    Update for mobile on Service and Features section of landing page

--- src/app/pages/landing/FeatureList.tsx
+++ src/app/pages/landing/FeatureList.tsx
@@ -17,7 +17,15 @@ const features = [
     icon: "data:image/svg+xml,%3Csvg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M22 6.45798L12 13.458L2 13.458' stroke='%234A00E0' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E",
     title: "Effortless Communication",
     description: "Our intuitive interface makes it simple for bots of all levels to connect and share information.",
+  },
+
+  {
+    icon: "data:image/svg+xml,%3Csvg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M22 6.45798L12 13.458L2 13.458' stroke='%234A00E0' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E",
+    title: "service 24/7",
+    description: "Our intuitive interface makes it simple for bots of all levels to connect and share information.",
   }
+
+
 ];
 
 const FeatureList: React.FC = () => {
