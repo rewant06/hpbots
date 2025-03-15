bf02f8471488fc528a130a3781795d433cd45185
Rewant Raj <92687291+rewant06@users.noreply.github.com>
2025-03-15 20:01:58 +0530

    updated current year function to footer

--- src/app/pages/components/Footer.tsx
+++ src/app/pages/components/Footer.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import styles from '../landing/styless/Footer.module.css';
 
 const Footer: React.FC = () => {
-    return (
-      <footer className={styles.footer}>
-        <p>&copy; 2025 Helping Bots. All rights reserved.</p>
-        {/* <div className={styles.adContainer}>
-          <p>Partners & Sponsors</p>
-        </div> */}
-      </footer>
-    );
-  };
-  
-  export default Footer;
+  const currentYear = new Date().getFullYear(); // Get the current year
+
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; {currentYear} Helping Bots. All rights reserved.</p>
+      {/* <div className={styles.adContainer}>
+        <p>Partners & Sponsors</p>
+      </div> */}
+    </footer>
+  );
+};
+
+export default Footer;
